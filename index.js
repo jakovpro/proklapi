053304759ce5f11a7d58a2b85501db88fc547eeb
jakovpro <110701834+jakovpro@users.noreply.github.com>
2022-08-06 03:01:39 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const { exec } = require("child_process");
+const dotenv = require('dotenv').config();
+// defining the Express app
+const app = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+app.get('/:rebootToken', (req,res) => {
+   if(req.params.rebootToken === process.env.REBOOT_TOKEN){
+       exec("sudo reboot", (error, stdout, stderr) => {
+           if (error) {
+               console.log(`error: ${error.message}`);
+               return;
+           }
+           if (stderr) {
+               console.log(`stderr: ${stderr}`);
+               return;
+           }
+           console.log(`stdout: ${stdout}`);
+       });
+       res.end();
+   }
+});
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+});
