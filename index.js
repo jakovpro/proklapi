9cbe25331ccfd2051c2253d6d790ae7bc8ec4e07
jakovpro <110701834+jakovpro@users.noreply.github.com>
2022-12-15 17:27:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ app.get('/reboot/:apiKey', (req,res) => {
    }
 });
 
+app.get('/logs/flush/:apiKey', (req,res) => {
+   if(req.params.apiKey === process.env.API_TOKEN){
+       exec("pm2 flush", (error, stdout, stderr) => {
+           if (error) {
+               console.log(`error: ${error.message}`);
+               return;
+           }
+		   
+           if (stderr) {
+               console.log(`stderr: ${stderr}`);
+               return;
+           }
+           console.log(`stdout: ${stdout}`);
+       });
+       res.end();
+   }
+});
+
 app.get('/allow/:newIp/:apiKey', (req,res) => {
     if(req.params.apiKey === process.env.API_TOKEN){
         exec(`ufw allow from ${req.params.newIp} to any port 80`, (error, stdout, stderr) => {
